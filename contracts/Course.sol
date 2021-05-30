36491bc551387513b64df85885ad3d0c6f7e7a57
Kevin Z <zkw917@gmail.com>
2021-05-30 10:07:30 -0400

    Initialize

--- contracts/Course.sol
+++ contracts/Course.sol
@@ -3,18 +3,39 @@ pragma solidity >0.5.2;
 contract Course {
   address public admin;
 
-  string public name;
+  string public _name;
 
   // courses
   mapping(address => string[]) credentials;
 
+  mapping(uint256 => address) _owners;
+
+  uint256 public _tokenCount;
+
   constructor() public {
     admin = msg.sender;
-    name = "Dogecation";
+    _name = "Dogecation";
+    _tokenCount = 1;
   }
+
+  event Transfer(
+    address indexed from,
+    address indexed to,
+    uint256 indexed tokenId
+  );
 
   modifier onlyAdmin() {
     require(msg.sender == admin);
     _;
+  }
+
+  function addTokenTo(address to, uint256 tokenId) private {
+    _owners[tokenId] = to;
+  }
+
+  function mint(address to, uint256 tokenId) public {
+    require(to != address(0));
+    addTokenTo(to, tokenId);
+    emit Transfer(address(0), to, tokenId);
   }
 }
